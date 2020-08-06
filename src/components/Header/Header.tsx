1e06522436ffe753a89a9de306dd411123bbd760
annaszalkiewicz <anna@mywebgraphicdesign.com>
2020-08-06 22:23:54 +0200

    Add header & styling

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Twenty-One Game</h1>
+      <p>Welcome, Anna!</p>
+    </header>
+  );
+}
+
+export default Header;
