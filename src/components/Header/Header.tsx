9755255a0e3e989d721692a813be923cb4a6508d
annaszalkiewicz <anna@mywebgraphicdesign.com>
2020-08-09 08:07:04 +0200

    Display user nickname in the header  after login

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './Header.scss';
+import { RootState } from '../../store/reducers/rootReducer';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
-  return (
-    <header className="header">
-      <h1>Twenty-One Game</h1>
-      <p>Welcome, Anna!</p>
-    </header>
-  );
-}
+	const user = (state: RootState) => state.user;
+	const props = useSelector(user);
+	return (
+		<header className='header'>
+			<h1>Twenty-One Game</h1>
+			{props.isLoggedIn && <p>Welcome, {props.nickname}!</p>}
+		</header>
+	);
+};
 
 export default Header;
